c500f6c9d90267fd5835eb0b23fb61b5f59e30bc
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-05-06 15:47:21 +0530

    uodates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ import Signin from './components/SignIn'
 import Error from './components/Error'
 import Privateroute from './components/Privateroute';
 
-
+const Content=({match})=>{
+ return <h2>{match.params.course}</h2>
+ 
+}
 
 class App extends React.Component {
 state={
@@ -43,11 +46,12 @@ registerhandleLogin=()=>{
             <Privateroute loggedin={this.state.loggedin} exact path='/startlearning' component={Startlearning}/>
             {/* <Privateroute loggedin={this.state.loggedin}   path='/about' component={About} /> */}
             {/* <Privateroute   loggedin={this.state.loggedin}       path='/contact' component={Contact} /> */}
-            <Route path='/courses' component={Courses}/>
+            <Route exact path='/courses' component={Courses}/>
             <Route path="/anything" render={()=><h1>anything</h1>}/>
             <Route path="/users/:username" component={Users}/>
             <Route path='/signup'render={props=><Signup {...props} click={this.registerhandleLogin} />}/>
             <Route path='/signin' render={(props)=><Signin {...props} click={this.registerhandleLogin}/>}/>
+            <Route  path="/courses/:course" component={Content }/>
             <Route component={Error} />
           </Switch>
         </header>
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,63 +1,63 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+
+const catalog=[
+    {
+     img:"https://webassets.mongodb.com/_com_assets/cms/mongodb-logo-rgb-j6w271g1xn.jpg",
+     name:"mongoDB",
+     cat:"Web Development"
+    },
+    {
+        img:"https://expressjs.com/images/express-facebook-share.png",
+        name:"Express JS",
+        cat:"Web Development"
+       },
+       {
+        img:"https://process.filestackapi.com/cache=expiry:max/resize=width:1050/compress/5KdzhvGRG61WMQhBa1Ql",
+        name:"React",
+        cat:"Web Development"
+       },
+       {
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png",
+        name:"React",
+        cat:"Web Development"
+       },
+]
 
 const Courses = () => {
-
+ 
     return (
-<div>
+        
+    <div>
         <header class="bg-primary text-center py-5 mb-4">
         <div class="container">
           <h1 class="font-weight-light text-white">Life Changing Courses</h1>
         </div>
       </header>
-      <div class="container">
-        <div class="row">
-          <div class=" col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-              <img src="https://webassets.mongodb.com/_com_assets/cms/mongodb-logo-rgb-j6w271g1xn.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">mongoDB</h5>
-                <div class="card-text text-black-50">Web Development</div>
+      <div class="grid-container">
+      <div class="row">
+       {catalog.map((i)=>
+          
+           
+              <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-0 shadow">
+                 <Link to={`/courses/${i.name}`}>
+                  <img src={i.img} class="card-img-top" height="120px" width="120px"alt="..."/>
+                  <div class="card-body text-center">
+                    <h5 class="card-title mb-0">{i.name}</h5>
+                    <div class="card-text text-black-50">{i.cat}</div>
+                  </div>
+                  </Link>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class=" col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-              <img src="https://expressjs.com/images/express-facebook-share.png" class="card-img-top" alt="..."/>
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">Express JS</h5>
-                <div class="card-text text-black-50">Web Development</div>
-              </div>
-            </div>
-          </div>
-         </div>
-         <div class="row">
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-              <img src="https://process.filestackapi.com/cache=expiry:max/resize=width:1050/compress/5KdzhvGRG61WMQhBa1Ql" class="card-img-top" alt="..."/>
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">React</h5>
-                <div class="card-text text-black-50">Web Development</div>
-              </div>
-            </div>
-          </div>
-         
-          <div class="col-xl-3 col-md-6 mb-4 ">
-            <div class="card border-0 shadow">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" class="card-img-top" alt="..."/>
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">Node JS</h5>
-                <div class="card-text text-black-50">Web Development</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      </div>
-    
- </div>
- 
-    )
-}
+              
+              
+             )}</div>
+             </div>
+             </div>
+             
+             
 
+    )}
         
 export default Courses;
